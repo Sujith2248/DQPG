f2e4e11c09b9682fcfe0bcf1b1c4fb955f1b1f77
Nithil <nithilpanangattiri@gmail.com>
2025-03-08 18:07:36 +0530

    update connection

--- connection.php
+++ connection.php
@@ -1,4 +1,15 @@
 <?php
+
+// $host = "localhost";
+//  $id = "root";
+//  $pass = "";
+//  $database = "dbexam";
+//  $user = "SiteAdmin";
+//  $errordb="Unable to select database";
+//  $error = "Unable to connect to the database check again..!!";
+// $conn = mysqli_connect($host,$id,$pass,$database) or die ($error);
+// mysql_select_db($database,$conn)or die ($errordb);
+
 // Database configuration
 $host = "localhost";    // Database host
 $dbname = "qpg_appdb"; // Database name
@@ -12,3 +23,5 @@ $conn = new mysqli($host, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Database connection failed: " . $conn->connect_error);
 }
+
+ ?>
