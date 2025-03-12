7b6450215a41e7a2a4d90d0c79d33b1417f9d4d4
Nithil <nithilpanangattiri@gmail.com>
2025-03-12 21:21:09 +0530

    update the code for redablility

--- users.php
+++ users.php
@@ -1,24 +1,25 @@
 <!DOCTYPE html>
 <?php
-session_start();
+// session_start();
 ?>
 <html>
-<head>
-    <title>users page</title>
-	<link rel="stylesheet" href="1.css" type="text/css">
-		
-		<script src="jquery-2.0.3.js">
 
-		</script>
-	<style>
-	  .dropdown {
+<head>
+  <title>users page</title>
+  <link rel="stylesheet" href="1.css" type="text/css">
+
+  <script src="jquery-2.0.3.js">
+
+  </script>
+  <style>
+    .dropdown {
       position: relative;
       display: inline-block;
     }
 
     .dropbtn {
       background-color: rgb(51, 150, 153);
-      border-radius:5px;
+      border-radius: 5px;
       color: white;
       padding: 10px;
       font-size: 20px;
@@ -32,7 +33,7 @@ session_start();
       right: 0;
       background-color: #f9f9f9;
       min-width: 160px;
-      box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+      box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
       z-index: 1;
     }
 
@@ -43,7 +44,9 @@ session_start();
       display: block;
     }
 
-    .dropdown-content a:hover {background-color: gainsboro}
+    .dropdown-content a:hover {
+      background-color: gainsboro
+    }
 
     .dropdown:hover .dropdown-content {
       display: block;
@@ -52,122 +55,138 @@ session_start();
     .dropdown:hover .dropbtn {
       background-color: rgb(26, 83, 93);
     }
-    .dropdown-content a:link, .dropdown-content a:visited {
-      text-decoration: none!important;
-      color:rgb(26, 83, 93);
+
+    .dropdown-content a:link,
+    .dropdown-content a:visited {
+      text-decoration: none !important;
+      color: rgb(26, 83, 93);
     }
+
     .dropdown:hover>.dropdown-content a:hover {
-      color:rgb(26, 83, 93);
+      color: rgb(26, 83, 93);
     }
 
     .bottom {
-        height :500px;
-        position:  inherit;
-        background-color: rgb(31, 104, 117);
-        color:white;
-        padding: 20px;
-        padding-left:60px;
-        margin: 10px 0px 0px 0px;
-    }
-    h4{
-        color:white;
-        font-size:2em;
-        margin-left:0%;
-        font-weight:lighter;
-    }
-    .foot{
-        letter-spacing:1px;
-        color:white;
-        padding:20px;
-        text-align:center;
-        background-color:rgb(26, 83, 93);
+      height: 500px;
+      position: inherit;
+      background-color: rgb(31, 104, 117);
+      color: white;
+      padding: 20px;
+      padding-left: 60px;
+      margin: 10px 0px 0px 0px;
     }
 
-	h2{
-		margin-top:50px;
-		font-weight:lighter;
-	}
-	#customers {
-		font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
-		border-collapse: collapse;
-		width: 100%;
-	}
+    h4 {
+      color: white;
+      font-size: 2em;
+      margin-left: 0%;
+      font-weight: lighter;
+    }
 
-	#customers td, #customers th {
-		border: 1px solid #ddd;
-		padding: 8px;
-	}
+    .foot {
+      letter-spacing: 1px;
+      color: white;
+      padding: 20px;
+      text-align: center;
+      background-color: rgb(26, 83, 93);
+    }
 
-	#customers tr:nth-child(even){background-color: #f2f2f2;color:black;}
-	#customers tr:nth-child(odd){background-color: white;color:black;}
+    h2 {
+      margin-top: 50px;
+      font-weight: lighter;
+    }
 
-	#customers tr:hover {background-color: #ddd;}
+    #customers {
+      font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
+      border-collapse: collapse;
+      width: 100%;
+    }
 
-	#customers th {
-		padding-top: 12px;
-		padding-bottom: 12px;
-		text-align: left;
-		background-color: rgb(26, 83, 93);
-		color: white;
-	}
-	</style>
+    #customers td,
+    #customers th {
+      border: 1px solid #ddd;
+      padding: 8px;
+    }
+
+    #customers tr:nth-child(even) {
+      background-color: #f2f2f2;
+      color: black;
+    }
+
+    #customers tr:nth-child(odd) {
+      background-color: white;
+      color: black;
+    }
+
+    #customers tr:hover {
+      background-color: #ddd;
+    }
+
+    #customers th {
+      padding-top: 12px;
+      padding-bottom: 12px;
+      text-align: left;
+      background-color: rgb(26, 83, 93);
+      color: white;
+    }
+  </style>
 </head>
-	<body>
-<?php	
 
- include ('header.php');
-?> 
-<div class="bottom">
-  <h4>All Users Panel</h4>
-  <hr>
+<body>
   <?php
-	echo "<h2>List of All users..</h2>";
-	include ('connection.php');
 
-	$result = mysqli_query($conn, "SELECT id, first_name, last_name, phone_number, email, role, address, gender, status FROM users");
-
-if (!$result) {
-    die("Query failed: " . mysqli_error($conn));
-}
-
-echo "<table id='customers' border='1' cellspacing='0' cellpadding='10'>
-        <tr> 
-            <th>First Name</th> 
-            <th>Last Name</th> 
-            <th>Contact</th>
-            <th>Email</th>
-            <th>Role</th>
-            <th>Address</th>
-            <th>Gender</th>
-            <th>Status</th>
-            <th>Options</th>
-        </tr>";
-
-while ($row = mysqli_fetch_assoc($result)) {
-    echo "<tr>
-             <td>" . (!empty($row['first_name']) ? $row['first_name'] : '') . "</td>
-              <td>" . (!empty($row['last_name']) ? $row['last_name'] : '') . "</td>
-              <td>" . (!empty($row['phone_number']) ? $row['phone_number'] : '') . "</td>
-              <td>" . (!empty($row['email']) ? $row['email'] : '') . "</td>
-              <td>" . (!empty($row['role']) ? $row['role'] : '') . "</td>
-              <td>" . (!empty($row['address']) ? $row['address'] : '') . "</td>
-              <td>" . (!empty($row['gender']) ? $row['gender'] : '') . "</td>
-              <td>" . (!empty($row['status']) ? 'Active' : 'Inactive') . "</td>
-             <td>
-                <a href='' style='color: blue; text-decoration: none;'>Edit</a> | 
-                <a href='' style='color: red; text-decoration: none;' onclick='return confirm(\"Are you sure you want to delete this user?\");'>Delete</a>
-            </td>
-          </tr>";
-}
-
-echo "</table>";
-	
+  include('header.php');
   ?>
-</div>
+  <div class="bottom">
+    <h4>All Users Panel</h4>
+    <hr>
+    <?php
+    echo "<h2>List of All users..</h2>";
+    include('connection.php');
 
-<div class="foot">
+    $result = mysqli_query($conn, "SELECT id, first_name, last_name, phone_number, email, role, address, gender, status FROM users");
+
+    if (!$result) {
+      die("Query failed: " . mysqli_error($conn));
+    }
+
+    // Function to handle empty values
+    function displayValue($value)
+    {
+      return !empty($value) ? htmlspecialchars($value) : '';
+    }
+
+    // Fetch field names dynamically
+    $fields = ['first_name' => 'First Name', 'last_name' => 'Last Name', 'phone_number' => 'Contact', 'email' => 'Email', 'role' => 'Role', 'address' => 'Address', 'gender' => 'Gender', 'status' => 'Status'];
+
+    ?>
+
+    <table id="customers" border="1" cellspacing="0" cellpadding="10">
+      <tr>
+        <?php foreach ($fields as $field => $label) : ?>
+          <th><?= $label ?></th>
+        <?php endforeach; ?>
+        <th>Actions</th>
+      </tr>
+
+      <?php while ($row = mysqli_fetch_assoc($result)) : ?>
+        <tr>
+          <?php foreach ($fields as $field => $label) : ?>
+            <td><?= ($field == 'status') ? ($row[$field] ? 'Active' : 'Inactive') : displayValue($row[$field]) ?></td>
+          <?php endforeach; ?>
+          <td>
+            <a href="" style="color: blue; text-decoration: none;">Edit</a> |
+            <a href="" style="color: red; text-decoration: none;" onclick="return confirm('Are you sure you want to delete this user?');">Delete</a>
+          </td>
+        </tr>
+      <?php endwhile; ?>
+    </table>
+  </div>
+
+  <div class="foot">
     Made With <img src="Vector.svg"> By CSE Techies Of KIET-W
-</div>
+  </div>
 
 </body>
+
 </html>
